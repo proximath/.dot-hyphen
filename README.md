7dbdf793894ebdf986979050700fe336045fa7bd
Proximath <quazitivity556@gmail.com>
2026-03-21 03:27:40 +0800

    add missing file

---
+++ README.md
@@ -0,0 +1,29 @@
+# .dot-hyphen
+
+**.dot-hyphen** is an esoteric programming language created by me in 2026. The language came to be as a result of me toying with the fact that $-(-x)=+x$. The language also draws some inspiration from [Lambda Calculus](https://esolangs.org/wiki/Lambda_calculus) for the philosophy that everything is an expression, although as you'll see, the language is inherently impure unlike most calculus-like languages.
+
+# Memory Model
+
+**.dot-hyphen** operates of a single unbounded-length array consisting of unbounded-valued **signed** integers, all of which are initialized to `0`. The address space of the array can be negative. 
+
+# Syntax and Semantics
+
+The language only has two syntax constructions, both of which are expressions:
+
+* `.x-y`: Can be interpreted as `array[x] -= y; return array[x]` in other languages. The order of evaluation is `x`, then `y`, then `.x-y`.
+* Non-negative integers: `0`, `1`, `2`, ...
+
+The entire program is basically a giant nested-expression made using these two constructions.
+
+The root expression is evaluated nonstop. At each evaluation, if the result of the root expression is within the ASCII range, it is outputted as an ASCII character.
+
+# Get Started
+
+Write a `.dot-hyphen` code with your favorite text-editor (or directly in the terminal if you're a psycho). Then, feed it to the interpreter like follows
+```sh
+python dothyphen.py < source-file.dot-hyphen
+```
+
+There are also some options you can try:
+* `-d` or `--debug`: print integers instead of ASCII characters and ignore the ASCII range limitation for outputs.
+* `-l` or `--limit`: takes an argument `n`. Limit execution to `n` iterations.
---
+++ dothyphen.py
@@ -0,0 +1,94 @@
+'''
+Interpreter for the language .dot-hyphen
+By Proximath
+
+Usage:
+
+Code in the terminal
+> echo "your code here" | python dothyphen.py
+
+Read from file
+> cat source-file.dot-hyphen | python dothyphen.py
+
+Limit to n iterations
+> echo "your code here" | python dothyphen.py --limit n
+
+Debugging, print as integers, not ascii characters
+> echo "your code here" | python dothyphen.py --debug
+'''
+
+from collections import defaultdict
+import sys
+from typing_extensions import Self
+import argparse
+
+sys.setrecursionlimit(100000)
+
+arr: defaultdict[int, int] = defaultdict(lambda: 0)
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-l", "--limit", type=int)
+argparser.add_argument("-d", "--debug", action="store_true")
+
+class Expr:
+    left: Self | None
+    right: Self | None
+    num: int = -1
+
+    def evaluate(self) -> int:
+        if self.num != -1:
+            return self.num
+        elif self.left is not None and self.right is not None:
+            x = self.left.evaluate()
+            y = self.right.evaluate()
+            arr[x] -= y
+            return arr[x]
+        else:
+            raise RuntimeError(f"INTERNAL ERROR: Something's wrong")
+
+def parse(code: str, index: int) -> tuple[Expr, int]:
+    while index < len(code) and code[index] not in ['.', '-'] and not code[index].isdigit():
+        index += 1
+    if index >= len(code):
+        print(f"SYNTAX ERROR: Expected expression at end of string")
+        sys.exit(1)
+    ret = Expr()
+    if code[index].isdigit():
+        ret.num = 0
+        while index < len(code) and code[index].isdigit():
+            ret.num = ret.num * 10 + int(code[index])
+            index += 1
+    elif code[index] == '.':
+        ret.left, index = parse(code, index + 1)
+        if index >= len(code):
+            print(f"SYNTAX ERROR: Expected '-' at end of string")
+            sys.exit(1)
+        elif code[index] != '-':
+            print(f"SYNTAX ERROR: Expected '-' at character #{index+1}")
+            sys.exit(1)
+        ret.right, index = parse(code, index + 1)
+    else:
+        print(f"SYNTAX ERROR: Unexpected {code[index]} at character #{index+1}")
+        sys.exit(1)
+    return ret, index
+
+def main():
+    args = argparser.parse_args()
+    limit = args.limit
+    debug_mode = args.debug
+    code = sys.stdin.read()
+    expr, index = parse(code, 0)
+    while index < len(code) and code[index] not in ['.', '-'] and not code[index].isdigit():
+        index += 1
+    if index < len(code):
+        print(f"SYNTAX ERROR: Unexpected {code[index]} at character #{index+1}")
+        sys.exit(1)
+    iteration = 0
+    while limit is None or iteration < limit:
+        res = expr.evaluate()
+        if 0 <= res < 128 and not debug_mode:
+            print(chr(res), end="")
+        elif debug_mode:
+            print(res, end="")
+        iteration += 1
+
+main()
